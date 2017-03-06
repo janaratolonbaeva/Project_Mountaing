48f9f000c13688daa632e8d2aad85586e00bd27c
janara <janaratolonbaeva@gmail.com>
2017-03-06 14:46:23 +0600

    add files

---
+++ vendor/js/main.js
@@ -0,0 +1,12 @@
+$('#popup-open').click(function () {
+    $('.wrap-popup').fadeIn();
+});
+
+
+
+// var validation = $('#reg-form').validate({
+    
+//     }
+// });
+
+// $('#phone').mask('+(999)999-99-99-99')
